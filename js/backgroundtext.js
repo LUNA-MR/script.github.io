fdb71739680da5d0d2b308134362c02a955181a6
LUNA-MR <88864839+LUNA-MR@users.noreply.github.com>
2021-10-31 08:29:19 +0900

    inital commit

---
+++ js/backgroundtext.js
@@ -0,0 +1,182 @@
+
+    const quotes = [
+        
+        {
+            quote: " 잡다한 세상일에 마음 쓰면 공부한다 해도 기초가 다져지지 않는다.\n ",
+            author: " - 이이(1536-1584) ",
+        },
+        {
+            quote: " 가장 강한 적은 자신이다.\n ",
+            author: " - 아베베(1922-1972)\n에디오피아의 마라톤 선수 ",
+        },
+        {
+            quote: " 겨울이 오면 봄 또한 멀지 않으리\n ",
+            author: " - 셀리(1792-1822)\n영국의 시인 ",
+        },
+        {
+            quote: " 사람은 먹기 위해 사는 것이 아니라 살기 위해 먹는다.\n ",
+            author: " - 소크라테스(BC469-399)\n그리스의 철학자 ",
+        },
+        {
+            quote: " 화가 나면 열까지 세라. \n",
+            author: " - 토마스 제퍼슨(1743-1826)\n미국 3대 대통령 ",
+        },
+        {
+            quote: " 주사위는 던저졌다.\n ",
+            author: " - 카이사르(BC100-44)\n로마의 정치가. 장군. 로마의 3두정치가(폼페이우스, 크라수스, 카이사르)의 한사람 ",
+        },
+        {
+            quote: " 책을 백독하면 저절로 알게 된다. \n",
+            author: " - 진수(233-297)\n중국 서진의 역사가. 삼국지의 원저자.  ",
+        },
+        {
+            quote: " 로마는 하루 아침에 이루어지지 않았다.\n ",
+            author: " - 세르반테스(1547-1616)\n스페인의 소설가, 극작가, 저서로 동키호테가 유명.  ",
+        },
+        {
+            quote: " 최고가 아니면 아무것도 만들지 않겠다.\n ",
+            author: " - 카네기(1835-1919)\n미국의 사업가, 미국 철강의 4분의 1이 그의 공장에서 최우수 제품으로 만들어졌다.  ",
+        },
+        {
+            quote:"성공의 비결은 좌절하지 않고 극복하는데 있다. \n",
+            author: " - 발자크(1799-1850)\n프랑스의 소설가.소설계의 셰익스피어 ",
+        },
+        {
+            quote: "인생에서 가장 중요한 일은 자기를 발견하는 것이다. \n 인간은 자기 자신에 대해 탐험 하는 것이 가장 중요하다.\n ",
+            author: " - 난센(1861-1930)\n 노르웨이 탐험가 ",
+        },
+        {
+            quote: "공상은 지식보다 중요하다. \n",
+            author: " - 아인슈타인(1879-1955) ",
+        },
+        {
+            quote: "일이 불가능하다고 믿는 것은 일을 불가능하게 하는 일이다.\n ",
+            author: " - 풀러(1608-1661) \n영국의 학자. 설교가 ",
+        },
+        {
+            quote:" 사람은 노력하고 있는 동안 방황하지 않는 법\n",
+            author: " - 괴테(1749-1832)\n독일의 시인 ",
+        },
+        {
+            quote: "잘못을 변명하면 그 잘못이 더 두드러진다.\n ",
+            author: " - 세익스피어(1564-1615)\n영국의 극작가, 시인",
+        },
+        {
+            quote: "뒤 돌아 보지 마라. 중요한 것은 미래다.\n ",
+            author: " - 칼 힐티(1833-1909)\n스위스의 법률가. 사상가.'잠 못 이루는 밥을 위하여','행복론' ",
+        },
+        {
+            quote: "천재란 1퍼센트의 영감과 99퍼센트의 땀으로 이루어진다. \n",
+            author: " - 에디슨(1847-1932)\n 학력 : 초등학교 3개월\n전등, 영사기, 축음기 등 ",
+        },
+        {
+            quote: " 고통 뒤의 즐거움은 달콤하다.\n",
+            author: " - 존 드라이든(1631-1700)\n영국의 시인. 극작가.영국 비평문학의 아버지 ",
+        },
+        {
+            quote: "여가란 무엇인가? 유익한 일을 하기 위한 시간이다.\n하루 24시간을 아껴 쓰는 사람만이 남들보다 앞서 간다.\n ",
+            author: " - 벤자민 플랭클린(1706-1790)\n미국의 정치가, 과학자 ",
+        },
+        {
+            quote: "천재는 인내다.\n ",
+            author: " - 뷔풍(1797-1788)\n프랑스의 철학자 ",
+        },
+        {
+            quote: " 인생은 짧고 예술은 길다.그러니 열심히 공부해라.\n",
+            author: " - 히포크라테스(BC 460-375)\n그리스의 의학자 ",
+        },
+        {
+            quote: "많은 일을 하고자 하면 지금 당장 한 가지 일을 시작하라.\n ",
+            author: " - 로스차일드(1743-1812)\n유대인 은행가 ",
+        },
+        {
+            quote: "아름다움은 모든 곳에 있다.\n ",
+            author: " - 로댕(1840-1917)\n프랑스의 조각가",
+        },
+        {
+            quote: "은혜를 베풀되 보답은 바라지 말며 준 뒤에는 후회하지 말자.\n",
+            author: " - 원효(617-686)\n신라의 승려",
+        },
+         
+        {
+          quote: "직업에 귀천은 없지만, 먹고 살 정도의 경제력은 갖추어야 한다.\n",
+          author: " - 郑太境",
+        },
+        {
+          quote:
+            "선행을 베풀되 0순위는 언제나 자신이어야 한다.\n",
+          author: " - 郑太境",
+        },
+        {
+          quote: "착하게 살아 당하느니 이기적으로 행동하여 살아남자\n",
+          author: " - 郑太境",
+        },
+        {
+          quote: "여아일어중만금\n",
+          author: " - 고3담임선생님",
+        },
+        {
+          quote: "학식도 미덕도 건강이 없으면 퇴색한다.\n ",
+          author: " - 몽테뉴(1533-1592)\n프랑스의 사상가. 도덕주의자. 르네상스를 대표하는 철학자. 문학가",
+        },
+        {
+          quote: "You only live once, but if you do it right, once is enough.\n",
+          author: " - Mae West",
+        },
+        {
+          quote: "Never go on trips with anyone you do ntot love.\n",
+          author: " - Hemmingway",
+        },
+        {
+          quote: "We wander for distraction, but we travel for fulfilment.\n",
+          author: " - Hilaire Belloc",
+        },
+        {
+          quote: "Travel expands the mind and fills the gap.\n",
+          author: " - Sheda Savage",
+        },
+      ];
+      const quote = document.querySelector("#quote span:first-child");
+      const author = document.querySelector("#quote span:last-child");
+      const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      
+      quote.innerText = todaysQuote.quote;
+      author.innerText = todaysQuote.author;
+      
+      const colors = [
+        "#ef5777",
+        "#575fcf",
+        "#4bcffa",
+        "#34e7e4",
+        "#0be881",
+        "#f53b57",
+        "#3c40c6",
+        "#0fbcf9",
+        "#00d8d6",
+        "#05c46b",
+        "#ffc048",
+        "#ffdd59",
+        "#ff5e57",
+        "#d2dae2",
+        "#485460",
+        "#ffa801",
+        "#ffd32a",
+        "#ff3f34"
+      ];
+      const btn = document.querySelector("button");
+      const body = document.querySelector("body");
+      const random1 = Math.floor(Math.random() * colors.length);
+      const random2 = Math.floor(Math.random() * colors.length);
+      body.style.background = `linear-gradient(to left, ${colors[random1]} , ${colors[random2]})`;
+      
+     
+      const images = [ "1.jpeg", "2.jpeg","3.jpeg","4.jpeg", "goal.jpeg","goal01.jpeg","verawang.jpeg"];
+      
+      const chosenImage = images[Math.floor(Math.random() * images.length)];
+      
+      const bgImage = document.createElement("img");
+     // const bgImage = document.querySelector(".img");
+      
+      bgImage.src = `img/${chosenImage}`;
+      
+      document.body.appendChild(bgImage);
